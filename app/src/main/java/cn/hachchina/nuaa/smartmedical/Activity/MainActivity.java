28351853cafa4e795d01134430bcb05c5f1150b7
DmrfCoder <xuefanggang97@gmail.com>
2018-05-16 23:25:30 +0800

    1:init main_activity_layout.xml 2:init project structure

---
+++ app/src/main/java/cn/hachchina/nuaa/smartmedical/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.hachchina.nuaa.smartmedical.Activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.hachchina.nuaa.smartmedical.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+    }
+}
